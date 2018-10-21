3e19606f753f7e4c328284a06f8c2bf0dbb302a5
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-10-21 20:25:46 +0000

    Display the reviews in reverse chronological order; Only allow logged in users to create reviews (disable/hide the form)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,26 @@
 class ReviewsController < ApplicationController
+	before_filter :require_login
 
-def create
-  @review = Review.new(review_params)
-  @review.product_id = params[:product_id]
-  @review.user = current_user
+	def create
+	  @review = Review.new(review_params)
+	  @review.product_id = params[:product_id]
+	  @review.user = current_user
 
-  if @review.save
-	  redirect_to product_path(@review.product)
-	else
-	  redirect_to @review.product
+	  if @review.save
+		  redirect_to product_path(@review.product)
+		else
+		  redirect_to @review.product
+		end
 	end
-end
 
 	private
+
+	def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to create reviews"
+      redirect_to login_path
+    end
+  end
 
 	def review_params
 	  params.require(:review).permit(:description, :rating)
